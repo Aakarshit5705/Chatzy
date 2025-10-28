9f06496266992f437907e6811585bda708fea489
Aakarshit <msd40551@gmail.com>
2025-10-28 14:38:45 +0530

    ready for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,26 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import authRouter from './routes/auth.routes';
+import path from 'path';
+
+import authRouter from './routes/auth.routes.js';
 
 dotenv.config();
 
 const PORT=process.env.PORT||3000;
 
+const __dirname=path.resolve();
+
 const app=express();
 
 app.use('/auth',authRouter);
+
+if(process.env.NODE_ENV=== "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+}
+
+app.get(/.*/,(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend/dist/index.html"))
+});
 
 app.listen(PORT,()=>{
     console.log('Server is listening at 3000 ');
