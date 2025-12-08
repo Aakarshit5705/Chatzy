8f709928ba7e61fdf854333827536083da016ded
Aakarshit <msd40551@gmail.com>
2025-12-08 17:36:27 +0530

    errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,9 @@ import React, { useEffect } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import {Toaster} from "react-hot-toast"
 
-import ChatPage from './pages/ChatPage'
-import SignupPage from './pages/signupPage'
-import LoginPage from './pages/LoginPage'
+import ChatPage from './pages/ChatPage.jsx'
+import SignupPage from './pages/SignupPage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
 import { useAuth } from './store/useAuthStore.js'
 import PageLoader from './components/PageLoader.jsx'
 
